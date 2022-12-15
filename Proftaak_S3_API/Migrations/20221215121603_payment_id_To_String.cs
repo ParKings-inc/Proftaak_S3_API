c0c32139283d226b5bdd17056817717d2b993ad8
davey2206 <davey2206@live.nl>
2022-12-15 13:16:26 +0100

    payment_id to string

---
+++ Proftaak_S3_API/Migrations/20221215121603_payment_id_To_String.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proftaak_S3_API.Migrations
+{
+    public partial class payment_id_To_String : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "payment_id",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "payment_id",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
